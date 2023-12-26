27b3093cd7929d6d53693620991576e04c9dd654
Unique-Usman <86585626+Unique-Usman@users.noreply.github.com>
2023-12-26 10:03:24 +0000

    Added skill portion

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Usman Akinyemi",
+  title: "Linux Lover",
+  email: "usmanakinyemi202@gmail.com",
+  gitHub: "https://github.com/Unique-Usman",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/usman-akinyemi-1689711b3/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/usman_akinyemi",
+  youTube: "Usman Akinyemi",
 };
 
 const primaryColor = "#4E567E";
